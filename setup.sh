8c9c4c56021847daa8ff29ad6288f22e3f013d12
Pierre Paridans <pierre@paridans.org>
2020-06-18 17:13:47 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+#
+## Fetch sub modules
+#
+git submodule init
+git submodule update
+
+#
+## Install base utilities
+#
+sudo apt install -y vim ack git curl xsel tig ruby build-essential htop zsh neofetch
+
+#
+## Symlinks config files/directories
+#
+ln -sr .ackrc $HOME/
+ln -sr .oh-my-zsh/plugins/odoo $HOME/.oh-my-zsh/plugins/
+# ln -sr .oh-my-zsh/plugins/nodenv $HOME/.oh-my-zsh/plugins/
+ln -sr .oh-my-zsh/plugins/pasteboard $HOME/.oh-my-zsh/plugins/
+ln -sr .gitconfig $HOME/
+ln -sr .vimrc $HOME/
+ln -sr .vim $HOME/
