911b0c3167d26a10383ea42845b900f63eadf78d
Pierre Paridans <pierre@paridans.org>
2022-02-03 15:34:42 +0100

    Pasteboard: to functions

--- config/fish/conf.d/pasteboard.fish
+++
@@ -1,6 +0,0 @@
-if ! type -q xsel
-    echo "Couldn't enable PasteBoard aliases ; 'apt install xsel' to enable them."
-else
-    alias pbcopy="xsel --clipboard --input"
-    alias pbpaste="xsel --clipboard --output"
-end
