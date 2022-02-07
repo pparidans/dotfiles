58c231c9ee66487dfa347f1477c926fdc3b41091
Pierre Paridans <pierre@paridans.org>
2022-02-07 22:55:05 +0100

    Fish: simplify functions "--wraps"

--- config/fish/functions/pbcopy.fish
+++ config/fish/functions/pbcopy.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function pbcopy --wraps='xsel --clipboard --input' --description 'alias pbcopy=xsel --clipboard --input'
+function pbcopy --wraps='xsel' --description 'alias pbcopy=xsel --clipboard --input'
   xsel --clipboard --input $argv;
 end
--- config/fish/functions/pbpaste.fish
+++ config/fish/functions/pbpaste.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function pbpaste --wraps='xsel --clipboard --output' --description 'alias pbpaste=xsel --clipboard --output'
+function pbpaste --wraps='xsel' --description 'alias pbpaste=xsel --clipboard --output'
   xsel --clipboard --output $argv;
 end
