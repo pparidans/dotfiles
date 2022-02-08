3f28dbeeff9d777c70468d1efce9957b3580aafa
Pierre Paridans <pierre@paridans.org>
2022-02-08 10:32:44 +0100

    Odoo: cleanup odorun alias

--- config/fish/functions/odorun.fish
+++ config/fish/functions/odorun.fish
@@ -1,4 +1,15 @@
 # Defined in - @ line 1
 function odorun --wraps='docker run' --description 'Run Odoo server in Docker dev environment'
-  docker run -it --rm -p 8069:8069 -v (realpath .):/src -v /var/tmp/odoo:/var/lib/odoo -v /var/run/postgresql:/var/run/postgresql -v /etc/passwd:/etc/passwd --user (id -u):(id -u) odoo-local-dev --dev xml,qweb,reload $argv;
+    docker run \
+        --interactive \
+        --tty \
+        --rm \
+        --publish 8069:8069 \
+        --volume (realpath .):/src \
+        --volume /var/tmp/odoo:/var/lib/odoo \
+        --volume /var/run/postgresql:/var/run/postgresql \
+        --volume /etc/passwd:/etc/passwd \
+        --user (id -u):(id -u) \
+        odoo-local-dev \
+        $argv;
 end
