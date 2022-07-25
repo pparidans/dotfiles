ec20d95bc4cea2403e66da4b9beddfc0317852e0
Pierre Paridans <pierre@paridans.org>
2022-07-25 11:37:59 +0200

    Fish: remove unused `dcrun` function

--- config/fish/functions/dcrun.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function dcrun --wraps='docker-compose run --service-ports' --description 'alias dcrun=docker-compose run --service-ports'
-  docker-compose run --service-ports $argv;
-end
