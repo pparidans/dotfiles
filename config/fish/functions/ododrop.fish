ecfbd95ef6d42ca583fabe212484cfb36931080c
Pierre Paridans <pierre@paridans.org>
2022-07-25 11:38:40 +0200

    Odoo: don't use per-branch database anymore

--- config/fish/functions/ododrop.fish
+++ config/fish/functions/ododrop.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ododrop --wraps='dropdb' --description 'Drop Odoo Postgresql dev database (use current git branch)'
-  dropdb -i odoodb-(git --git-dir=odoo/.git branch --show-current) $argv;
+function ododrop --wraps='dropdb' --description 'Drop Odoo Postgresql dev database'
+  dropdb odoodb $argv;
 end
