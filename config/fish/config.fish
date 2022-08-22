df3321178765fd54ce287f0fb76685a42b3a201c
Pierre Paridans <pierre@paridans.org>
2022-08-22 14:33:55 +0200

    Fish: don't output when adding ~/.local/bin to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,7 @@ set fish_greeting
 # Add ~/.local/bin to PATH
 if type -q fish_add_path
     # Available since 3.2.0
-    fish_add_path -v -m ~/.local/bin
+    fish_add_path -m ~/.local/bin
 else
     # Fallback for older versions (e.g. Debian/Ubuntu are still on 3.1.0)
     contains (realpath ~/.local/bin) $fish_user_paths; or set -Ua fish_user_paths (realpath ~/.local/bin)
