2995af3fd9b943a03bb6b96a481689156120aba2
Pierre Paridans <pierre@paridans.org>
2023-09-12 10:40:10 +0200

    odoo: bookmarklets

---
+++ odoo/bookmarklets.js
@@ -0,0 +1,12 @@
+// Debug Assets mode
+javascript:(function () { let url = new URL(window.location.href);url.searchParams.set('debug', 'assets');window.location.href = url.toString()})()
+
+// Login
+javascript:(function () { const LOGIN_PATH = "/web/login"; if (location.pathname !== LOGIN_PATH) { location.pathname = LOGIN_PATH } else { for (let field of ["login", "password"]) { document.forms[0][field].value = "admin" };document.forms[0].submit() } })()
+
+// Server version
+javascript:(function () { fetch(`${new URL(window.location.href).origin}/web/webclient/version_info`,{headers:{"Content-Type":"application/json",},referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify({jsonrpc:"2.0",method:"call",id:"21321",params:{},}),method:"POST",}).then((response)=>response.json()).then(({result})=>{alert(`Odoo Version ${result.server_version}`);}).catch(()=>{alert('Cannot find Odoo Version');}) })()
+
+// Database list
+javascript:(function () { fetch(`${new URL(window.location.href).origin}/web/database/list`,{headers:{"Content-Type":"application/json",},referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify({jsonrpc:"2.0",method:"call",id:"21321",params:{},}),method:"POST",}).then((response)=>response.json()).then(({result})=>{alert(`Databases: ${result}`);}).catch(()=>{alert('Cannot find Odoo Databases');}) })()
+
