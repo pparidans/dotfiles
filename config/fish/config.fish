be8c26263d4facc678526145a993fc397fc04db0
Pierre Paridans <pierre@paridans.org>
2025-11-19 12:09:10 +0100

    fish: add dotfiles/bin to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+
+    # Add dotfiles' `bin` to PATH
+    set toplevel_path (realpath (readlink -f (status dirname))/../..)
+    fish_add_path --path "$toplevel_path/bin"
 end
